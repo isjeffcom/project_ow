bc1ff680a8bd492f643ce63f5eddeafbef68359c
JeffWu <isjeffcom@gmail.com>
2017-05-04 22:04:00 +0100

    delete a unused file

--- header.php
+++ header.php
@@ -3,8 +3,7 @@
   <head>
     <title>OverMark - DAMP UoP</title>
     <meta http-equiv="content-type" content="text/html" charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="./asset/css/style.css" >
-    <link rel="stylesheet" type="text/css" href="./asset/css/ani.css" >
+    <link rel="stylesheet" type="text/css" href="./asset/css/style.css?v=1.1" >
     <script src='./js/jq311.js'/></script>
     <script src="./js/chart.js"></script>
 
